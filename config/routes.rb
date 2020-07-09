86f689eac80277f9cca50388769e9cf45c7022b7
ikedayohei <yoheii0612@gmail.com>
2020-07-09 22:56:05 +0900

    rooute add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "estimates#index"
+  resources :estimates do
+    collection do
+      get 'search'
+    end
+  end
+  resources :users, only: [:show,:edit,:update]
 end
