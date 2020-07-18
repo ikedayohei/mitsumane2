346c87915f2c9f040e46da7f42435fd254c988d0
ikedayohei <yoheii0612@gmail.com>
2020-07-19 00:40:49 +0900

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "3.13.0"
 set :application, 'mitsumane2'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/ikedayohei/mitsumane2.git'
+set :repo_url,  'git@github.com:ikedayohei/mitsumane2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
