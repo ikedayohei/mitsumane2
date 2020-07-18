fd020ace32376ec1f371e384184fc9a3a4aa01ae
ikedayohei <yoheii0612@gmail.com>
2020-07-18 15:37:31 +0900

    devise add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,5 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
 end
---
+++ app/helpers/estimates_helper.rb
@@ -0,0 +1,5 @@
+module EstimatesHelper
+  def converting_to_jpy(price)
+    "#{price.to_s(:delimited, delimiter: ',')}円"
+  end
+end
